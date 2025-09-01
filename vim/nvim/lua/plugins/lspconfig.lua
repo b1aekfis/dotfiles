767deb579109585a5a066a3f524c621b6a25ef28
b1aekfis <148613625+b1aekfis@users.noreply.github.com>
2025-09-02 02:45:08 +0700

    refactor(nvim)!: update lspconfig
    BREAKING CHANGE: requires Neovim v0.11+, Mason and mason-lspconfig v2+

--- vim/nvim/lua/plugins/lspconfig.lua
+++ vim/nvim/lua/plugins/lspconfig.lua
@@ -10,9 +10,6 @@ return {
         "williamboman/mason.nvim",
         opts = {} -- ensure setup
       },
-      opts = {
-        ensure_installed = { "clangd", "rust_analyzer", "lua_ls" },
-      }
     },
     -- blink.cmp
     { "saghen/blink.cmp" },
@@ -23,47 +20,41 @@ return {
     },
   },
   config = function()
-    local mason_lspconfig = require("mason-lspconfig")
-    local nvim_lsp = require("lspconfig")
     local capabilities = require('blink.cmp').get_lsp_capabilities()
 
-    mason_lspconfig.setup_handlers({
-      function(server)
-        nvim_lsp[server].setup({
-          capabilities = capabilities,
-        })
-      end,
-      ["clangd"] = function()
+    vim.lsp.config("clangd", {
+      capabilities = capabilities,
+      cmd = (function()
         local is_windows = vim.fn.has('win32') == 1
         local gxx_path = vim.fn.systemlist((is_windows and "where g++" or "which g++"))[1] or ""
-        local clangd_cmd = { 'clangd' }
+        local clangd_cmd = { "clangd" }
         if gxx_path ~= "" then
           table.insert(clangd_cmd, "--query-driver=" .. gxx_path)
         end
-        nvim_lsp["clangd"].setup({
-          capabilities = capabilities,
-          cmd = clangd_cmd,
-        })
-      end,
-      ["lua_ls"] = function()
-        nvim_lsp["lua_ls"].setup({
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = {
-                  'vim',
-                },
-              },
-            },
-          },
-          capabilities = capabilities,
-        })
-      end,
-      ["rust_analyzer"] = function()
-        nvim_lsp["rust_analyzer"].setup({
-          capabilities = capabilities,
-        })
-      end,
+        return clangd_cmd
+      end)(),
     })
+
+    vim.lsp.config("lua_ls", {
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
+
+    vim.lsp.config("rust_analyzer", {
+      capabilities = capabilities,
+    })
+
+    require("mason").setup()
+
+    require("mason-lspconfig").setup {
+      ensure_installed = { "clangd", "rust_analyzer", "lua_ls" },
+      automatic_enable = false
+    }
   end,
 }
